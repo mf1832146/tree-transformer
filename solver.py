b9edd3322bc8b9ce1fce035a2e2c22f9b321544b
tangze <mf1832146@smail.nju.edu.cn>
2019-12-22 16:25:55 +0800

    [mod] 修复bug

--- solver.py
+++ solver.py
@@ -140,13 +140,14 @@ def greedy_decode(tree_transformer_model, batch, max_len, start_pos):
                                               batch.re_par_ids,
                                               batch.re_bro_ids)
     print('______')
-    print('memory', memory)
+    print('code_mask', batch.code_mask)
     ys = torch.ones(1, 1).fill_(start_pos).type_as(batch.code.data)
     for i in range(max_len - 1):
         #  memory, code_mask, comment, comment_mask
         out, _, _ = tree_transformer_model.decode(memory, batch.code_mask,
                                                   Variable(ys),
                                                   Variable(subsequent_mask(ys.size(1)).type_as(batch.code.data)))
+        print('out', out)
         prob = tree_transformer_model.generator(out[:, -1])
         _, next_word = torch.max(prob, dim=1)
         next_word = next_word.data[0]
